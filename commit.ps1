1621699ece067c68f74c7f523078f282a2160732
ZICXR <2403057219@qq.com>
2026-03-21 11:21:59 +0800

    2026-03-21

---
+++ commit.ps1
@@ -0,0 +1,11 @@
+# 获取当日日期，格式为 YYYY-MM-DD
+$date = Get-Date -Format "yyyy-MM-dd"
+
+# 添加所有改动
+git add -A
+
+# 提交，提交信息为当日日期
+git commit -m $date
+
+# 推送到默认远程分支
+git push
--- 哈希/两数之和.cpp
+++ 哈希/两数之和.cpp
@@ -2,7 +2,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map<int, int> mp;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (mp.count(target - nums[i])) {
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+            } else {
+                mp[nums[i]] = i;
+            }
+        }
+        return ans;
     }
 };
 
+// 解法2 暴力枚举
