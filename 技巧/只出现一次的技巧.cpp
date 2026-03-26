2755a2a076a229a11cf65451575509ab9aff40a5
ZICXR <2403057219@qq.com>
2026-03-26 20:05:49 +0800

    2026-03-26

---
+++ 技巧/只出现一次的技巧.cpp
@@ -0,0 +1,10 @@
+// 解法1 位运算
+// a ^ a = 0   a ^ 0 = a
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i ++) ans ^= nums[i];
+        return ans;
+    }
+};
